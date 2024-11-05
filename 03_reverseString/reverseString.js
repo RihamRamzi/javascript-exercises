1975ced2ad7de35fd04d37659106f10507ba3677
RihamRamzi <rihamsoftware@gmail.com>
2024-11-05 16:33:01 +0530

    Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let strArray = str.split("");
+  strArray.reverse();
+  let strReversed = strArray.join("");
 
+  return strReversed;
 };
 
 // Do not edit below this line
