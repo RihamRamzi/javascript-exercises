ed51498d9785ef28327553ce2a5311d918f5f901
RihamRamzi <rihamsoftware@gmail.com>
2024-11-05 16:18:10 +0530

    Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, repeats) {
+  let result = "";
 
+  if (repeats < 0) {
+    result = "ERROR";
+  }
+
+  for (i = 0; i < repeats; i++) {
+    result += str;
+  }
+  return result;
 };
 
 // Do not edit below this line
