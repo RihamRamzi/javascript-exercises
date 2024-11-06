bd20be194e49f11827cabc0643866ca2fdb31dcb
RihamRamzi <rihamsoftware@gmail.com>
2024-11-06 14:00:35 +0530

    complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...multipleIndex) {
+  const newArray = array.filter((item) => !multipleIndex.includes(item));
+  return newArray;
 };
 
 // Do not edit below this line
